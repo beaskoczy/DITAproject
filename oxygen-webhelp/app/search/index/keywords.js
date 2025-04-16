a1f7f8783d2ec66e19b855045ff8a393b71e1cab
beaskoczy <beaskoczy@gmail.com>
2025-04-16 15:35:08 +0200

    Documentation setup to GitHub Pages

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,15 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"My",p:["p1","p2","p3"]},{w:"doc",p:["p1","p2","p3"]},{w:"Index",p:["p2"]},{w:"Terms",p:["p2"]},{w:"-",p:["p2","p3"]},{w:"Search",p:["p3"]},{w:"Results",p:["p3"]},{w:"Filters",p:["p4"]},{w:"c_new",p:["p5"]},{w:"concept",p:["p5"]},{w:"Setting",p:["p6"]},{w:"up",p:["p6"]},{w:"a",p:["p6"]},{w:"new",p:["p6"]},{w:"laptop",p:["p6"]},{w:"Task",p:["p7"]},{w:"2",p:["p7"]},{w:"Restart",p:["p8"]},{w:"your",p:["p8"]},{w:"machine",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 6, 7, 3, 4];
+ph["p3"]=[8, 9, 7, 3, 4];
+ph["p4"]=[10];
+ph["p5"]=[11, 12];
+ph["p6"]=[13, 14, 15, 16, 17];
+ph["p7"]=[18, 19];
+ph["p8"]=[20, 21, 22];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
